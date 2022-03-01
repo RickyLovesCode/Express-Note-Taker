f2d3fae8b5d73aaebcbea4e9ae474938b309d4f5
Ricky27 <Rickyrenteria7@gmail.com>
2022-02-28 17:35:47 -0800

    fixed index.route bug

--- routes/hmtlRoutes/index.js
+++ routes/hmtlRoutes/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", (res, req) => {
-    res.sendFile(path.join(__dirname, "../../Develop/assets/public/index.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Develop/public/assets/index.html"));
 });
 
-router.get("*", (res, req) => {
-    res.sendFile(path.join(__dirname, "../../Develop/assets/public/index.html"));
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Develop/public/assets/index.html"));
 });
-
-
 
 module.exports = router;
